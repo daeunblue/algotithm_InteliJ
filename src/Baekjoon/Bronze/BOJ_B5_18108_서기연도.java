19a89887b3e14c93461441ad3c4b0e07ecdc426d
daeunblue <57705970+daeunblue@users.noreply.github.com>
2022-04-17 23:52:46 +0900

    BOJ_B5_18108_서기연도[입출력]

---
+++ src/Baekjoon/Bronze/BOJ_B5_18108_서기연도.java
@@ -0,0 +1,15 @@
+package Baekjoon.Bronze;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class BOJ_B5_18108_서기연도 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine()) -543;
+		System.out.println(N);
+	}
+}
